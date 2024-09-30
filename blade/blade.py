3d8ce91e794162bdfa47f7ef68d87062506ed59c
ParkhomenkoDV <parkho.m.enko@mail.ru>
2024-09-30 18:46:01 +0300

    2024-09-30-18-45

--- blade/blade.py
+++ blade/blade.py
@@ -1,6 +1,6 @@
 from types import MappingProxyType  # неизменяемый словарь
 import numpy as np
-from numpy import nan, isnan, pi, sqrt, array, linspace, arange, radians
+from numpy import nan, isnan, pi, sqrt, exp, log as ln, array, linspace, arange, radians
 from scipy import interpolate, integrate
 import matplotlib.pyplot as plt
 
@@ -25,6 +25,8 @@ REFERENCES = MappingProxyType({
 
 class Blade:
     """Лопатка/винт/лопасть"""
+    __slots__ = ('__material', '__sections', '__bondages', '__height',
+                 '__area', '__f_area')
 
     @classmethod
     def help(cls):
@@ -32,7 +34,8 @@ class Blade:
         print('Расчет на прочность')
         return version
 
-    def __init__(self, material: Material, sections: dict[float | int | np.number: list, tuple, np.ndarray]) -> None:
+    def __init__(self, material: Material, sections: dict[float | int | np.number: list, tuple, np.ndarray],
+                 bondages=tuple()) -> None:
         # проверка на тип данных material
         assert isinstance(material, Material)
 
@@ -45,18 +48,64 @@ class Blade:
         assert all(isinstance(x, (int, float, np.number)) and isinstance(y, (int, float, np.number))
                    for el in sections.values() for x, y in el)
 
-        self.__material = material
+        assert isinstance(bondages, (tuple, list))
+        assert all(isinstance(bondage, dict) for bondage in bondages)
+        assert all('radius' in bondage.keys() and 'volume' in bondage.keys() for bondage in bondages)
+        assert all(isinstance(bondage['radius'], (int, float, np.number)) and
+                   isinstance(bondage['volume'], (int, float, np.number)) for bondage in bondages)
+        assert all(0 <= bondage['radius'] and 0 <= bondage['volume'] for bondage in bondages)
+
+        self.__material = material  # материал
         self.__sections = dict(sorted(sections.items(), key=lambda item: item[0]))  # сортировка по высоте
+        self.__bondages = bondages  # бондажи
 
         self.__height = max(self.__sections.keys()) - min(self.__sections.keys())
+
+        self.__area = dict()
+        for z, section in self.__sections.items():
+            upper_lower = self.upper_lower(section)
+            xu, yu = array(upper_lower['upper'], dtype='float32').T
+            xl, yl = array(upper_lower['lower'], dtype='float32').T
+            fu = interpolate.interp1d(xu, yu, kind=1, fill_value='extrapolate')
+            fl = interpolate.interp1d(xl, yl, kind=1, fill_value='extrapolate')
+            area_upper = integrate.quad(fu, xu[0], xu[-1], limit=len(xu), points=xu)[0]
+            area_lower = integrate.quad(fl, xl[0], xl[-1], limit=len(xl), points=xl)[0]
+            self.__area[z] = area_upper - area_lower
+        self.__f_area = interpolate.interp1d(list(self.__area.keys()), list(self.__area.values()),
+                                             kind=1, fill_value='extrapolate')
 
     @property
     def material(self):
         return self.__material
 
     @property
+    def sections(self):
+        return self.__sections
+
+    @property
+    def bondages(self):
+        return self.__bondages
+
+    @property
     def height(self) -> float:
         return self.__height
+
+    @staticmethod
+    def upper_lower(coordinates: tuple[tuple[float, float], ...]) -> dict[str:tuple[tuple[float, float], ...]]:
+        """Разделение координат на спинку и корыто"""
+        X, Y = array(coordinates).T
+        argmin, argmax = np.argmin(X), np.argmax(X)
+        upper, lower = list(), list()
+        if argmin < argmax:
+            for x, y in zip(X[argmax:-1:+1], Y[argmax:-1:+1]): upper.append((float(x), float(y)))
+            for x, y in zip(X[:argmin + 1:+1], Y[:argmin + 1:+1]): upper.append((float(x), float(y)))
+            for x, y in zip(X[argmin:argmax + 1:+1], Y[argmin:argmax + 1:+1]): lower.append((float(x), float(y)))
+        else:
+            for x, y in zip(X[argmax:argmin + 1:+1], Y[argmax:argmin + 1:+1]): upper.append((float(x), float(y)))
+            for x, y in zip(X[argmin:-1:+1], Y[argmin:-1:+1]): lower.append((float(x), float(y)))
+            for x, y in zip(X[:argmax + 1:+1], Y[:argmax + 1:+1]): lower.append((float(x), float(y)))
+        # if upper[-1][0] != 0: upper.append((0, ?)) # неизвестен y входной кромки
+        return {'upper': tuple(upper[::-1]), 'lower': tuple(lower)}
 
     def show(self, D: int, **kwargs):
         """Визуализация"""
@@ -88,8 +137,38 @@ class Blade:
 
         plt.show()
 
-    def tensions(self, show=True):
+    def equal_strength(self, rotation_frequency: float | int | np.number, temperature: float | int | np.number):
+        """Равнопрочность"""
+        assert isinstance(rotation_frequency, (int, float, np.number))
+        assert isinstance(temperature, (int, float, np.number)) and 0 < temperature
+
+        ksi = self.__area[-1] / self.__area[0]
+
+        radius0, *_, radius1 = list(self.__sections.keys())  # радиус втулки и периферии
+
+        radius_equal_strength = sqrt((radius0 ** 2 - radius1 ** 2 * ln(ksi)) / (1 - ln(ksi)))
+
+        sigma_equal_strength_max = 0.5 * self.material.density(temperature) * rotation_frequency ** 2
+        sigma_equal_strength_max *= (radius1 ** 2 - radius0 ** 2)
+
+        f_area_equal_strength = lambda z: \
+            (self.__area[-1] *
+             exp(self.material.density(temperature) * rotation_frequency ** 2 / sigma_equal_strength_max) *
+             integrate.quad(z, z, radius_equal_strength)) \
+                if z <= radius_equal_strength else self.__area[-1]
+
+        f_force_r_equal_strength = lambda z: \
+            (self.material.density(temperature) * rotation_frequency ** 2 *
+             (integrate.quad(f_area_equal_strength(z) * z, z, radius1) +
+              sum([b['radius'] * b['volume'] for b in self.bondages])))
+
+        return {'radius': radius_equal_strength, }
+
+    def tensions(self, rotation_frequency: float | int | np.number, pressure, density, show=True):
         """Расчет на прочность"""
+        assert isinstance(rotation_frequency, (float, int, np.number))
+
+        N = 0
 
         if show: self.__show_tensions()
         return
@@ -100,17 +179,17 @@ class Blade:
         plt.figure(figsize=(12, 8))
         plt.show()
 
-    def natural_frequencies(self, max_k: int) -> tuple[float, str]:
+    def natural_frequencies(self, radius: int, max_k: int) -> tuple[float, str]:
         """Частота собственных колебаний [6]"""
         self.I = self.b * self.c ** 3 / 12  # момент инерции сечения
         self.F = self.b * self.c  # площадь сечения
 
-        if "крепление в заделке":
+        if radius == 0:  # крепление в заделке
             k = array([1.875, 4.694] + [pi * (i - 0.5) for i in range(3, max_k + 1, 1)])
-        elif "крепление шарнирное":
+        elif radius == -1:  # крепление шарнирное
             k = array([0] + [pi * (i - 0.75) for i in range(2, max_k + 1, 1)])
         else:
-            raise
+            raise Exception(f'radius = 0 (крепление в заделке) or radius = -1 (крепление шарнирное)')
 
         f = self.material.E(0) * self.I / (self.material.density(0) * np.mean(self.F))
         f = sqrt(f)
@@ -123,7 +202,8 @@ class Blade:
         rotation_frequency = arange(0, max_rotation_frequency + 1, 1) * (2 * pi)  # перевод из рад/c в 1/c=об/c=Гц
         # динамическая частота колебаний вращающегося диска
         # self.R[0] = радиус корня
-        f = sqrt(self.natural_frequencies(max(multiplicity))[0] ** 2 + (1 + self.R[0] / self.h) * rotation_frequency ** 2)
+        f = sqrt(
+            self.natural_frequencies(max(multiplicity))[0] ** 2 + (1 + self.R[0] / self.h) * rotation_frequency ** 2)
         resonance = set()  # резонансные частоты [1/с]
 
         plt.figure(figsize=kwargs.pop('figsize', (8, 8)))
@@ -181,7 +261,23 @@ def test():
         blade.show(2)
         blade.show(3)
 
-        tensions = blade.tensions(show=True)
+        pressure = {0.5: (10 ** 5, 10 ** 5),
+                    0.6: (10 ** 5, 10 ** 5),
+                    0.7: (10 ** 5, 10 ** 5)}
+
+        density = {0.5: (10 ** 5, 10 ** 5),
+                   0.6: (10 ** 5, 10 ** 5),
+                   0.7: (10 ** 5, 10 ** 5)}
+
+        velocity_a = {0.5: (10 ** 5, 10 ** 5),
+                      0.6: (10 ** 5, 10 ** 5),
+                      0.7: (10 ** 5, 10 ** 5)}
+
+        velocity_t = {0.5: (10 ** 5, 10 ** 5),
+                      0.6: (10 ** 5, 10 ** 5),
+                      0.7: (10 ** 5, 10 ** 5)}
+
+        tensions = blade.tensions(2800, pressure=pressure, density=density, show=True)
 
 
 if __name__ == '__main__':
